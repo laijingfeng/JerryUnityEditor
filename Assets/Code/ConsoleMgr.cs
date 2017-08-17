7b51a131d19da4a6293ce7d5c228a8d3b878cfb5
laijingfeng <337292616@qq.com>
2017-08-17 16:20:04 +0800

    add ConsoleMgr

---
+++ Assets/Code/ConsoleMgr.cs
@@ -0,0 +1,12 @@
+﻿
+public static class ConsoleMgr
+{
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void ClearConsole()
+    {
+        // This simply does "LogEntries.Clear()" the long way:  
+        var logEntries = System.Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+        clearMethod.Invoke(null, null);
+    }
+}
