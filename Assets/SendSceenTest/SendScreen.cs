f993e6f5e969a1af73592094f52335349d325e1d
laijingfeng <337292616@qq.com>
2018-06-13 01:27:57 +0800

    add

---
+++ Assets/SendSceenTest/SendScreen.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendScreen : MonoBehaviour
+{
+    /// <summary>
+    /// 帧率
+    /// </summary>
+    [Tooltip("帧率")]
+    public int frameRate = 25;
+    public Camera m_Camera;
+    public RawImage m_TarImg;
+
+    private RenderTexture rt;
+    private int width;
+    private int height;
+    private Texture2D tex_output;
+    private Texture2D tex_input;
+    private Rect rect;
+
+    private void Start()
+    {
+        width = Screen.width;
+        height = Screen.height;
+        rt = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
+        tex_output = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        tex_input = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        rect = new Rect(0, 0, width, height);
+        this.StartCoroutine("IE_CaptureScreen");
+    }
+
+    private IEnumerator IE_CaptureScreen()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            CaptureOneFrame();
+        }
+    }
+
+    private void CaptureOneFrame()
+    {
+        //Debug.LogWarning("CaptureOneFrame");
+
+        m_Camera.targetTexture = rt;
+        m_Camera.Render();
+        RenderTexture.active = rt;
+        tex_output.ReadPixels(rect, 0, 0);
+        tex_output.Apply();
+        m_TarImg.texture = tex_output;
+        m_Camera.targetTexture = null;
+
+        //tex_output.GetPixels();
+    }
+}
