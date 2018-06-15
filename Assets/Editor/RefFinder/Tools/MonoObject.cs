b1bb92051dafba10a931b5f6b547d0696940a027
laijingfeng <337292616@qq.com>
2018-06-15 19:31:02 +0800

    测试

--- Assets/Editor/RefFinder/Tools/MonoObject.cs
+++ Assets/Editor/RefFinder/Tools/MonoObject.cs
@@ -12,7 +12,7 @@ public class MonoObject : FinderToolBaseObject
             SetTip(string.Format("目标对象不是查找对象的载体({0})", MyCarrierListStr()), MessageType.Warning);
             return;
         }
-
+        
         switch (type)
         {
             case FinderToolMgrBase.AssetType.GameObject:
@@ -29,14 +29,27 @@ public class MonoObject : FinderToolBaseObject
         SetTip(string.Format("查找结果如下({0}):", results.Count), MessageType.Info);
     }
 
+    /// <summary>
+    /// 在一个GameObject上查找脚本
+    /// </summary>
+    /// <param name="findObject"></param>
+    /// <param name="targetGo"></param>
+    /// <returns></returns>
     public static List<Object> DoOneGameObject(Object findObject, GameObject targetGo)
     {
+        MissingMonoCheck(targetGo);
+
         string findObjectType = findObject.name.ToString();
         Component[] coms = targetGo.GetComponentsInChildren<Component>(true);
         List<Object> ret = new List<Object>();
 
         foreach (Component com in coms)
         {
+            //空引用
+            if (com == null)
+            {
+                continue;
+            }
             if (com.GetType().ToString().Equals(findObjectType))
             {
                 ret.Add(com);
@@ -44,5 +57,34 @@ public class MonoObject : FinderToolBaseObject
         }
 
         return ret;
+    }
+
+    /// <summary>
+    /// 查找丢失或者无法加载的脚本
+    /// </summary>
+    static private void MissingMonoCheck(GameObject targetGo)
+    {
+        if(targetGo == null)
+        {
+            return;
+        }
+
+        Component[] coms = targetGo.GetComponents<Component>();
+        foreach (Component com in coms)
+        {
+            if (com == null)
+            {
+                UnityEngine.Debug.LogError("发现一个丢失或者无法加载的脚本", targetGo);
+            }
+        }
+
+        for (int i = 0, imax = targetGo.transform.childCount; i < imax; i++)
+        {
+            Transform child = targetGo.transform.GetChild(i);
+            if (child != null)
+            {
+                MissingMonoCheck(child.gameObject);
+            }
+        }
     }
 }
